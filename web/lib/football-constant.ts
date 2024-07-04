53fdfbd336bbd042e6414005ac2b5d360dfff087
chixx <lucottejeremie@gmail.com>
2024-07-04 16:04:48 +0200

    add lib/football-constant.ts

---
+++ web/lib/football-constant.ts
@@ -0,0 +1,6 @@
+export const winnerCondition =
+  "if(key1 > key2){return 1};if(key1 == key2){return 2};return 3;";
+
+export const maxDirection = 3;
+
+export const keysParams = ["response[0].goals.home", "response[0].goals.away"];
