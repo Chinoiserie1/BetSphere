e95ac5f63241993daaae0dab3e8b6d789135819b
chixx <lucottejeremie@gmail.com>
2024-10-03 12:14:18 +0200

    update home loyout and page

--- web/app/home/layout.tsx
+++ web/app/home/layout.tsx
@@ -1,5 +1,6 @@
 import { ContentLayout } from "@/components/admin-panel/content-layout";
 import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
+import HomeHeader from "@/components/header/home-header";
 
 type Props = {
   children: React.ReactNode;
@@ -9,7 +10,9 @@ export default function Layout({ children }: Props) {
   return (
     <div className="w-screen h-screen overflow-hidden">
       <AdminPanelLayout>
-        <ContentLayout title="Home">{children}</ContentLayout>
+        {/* <ContentLayout title="Home">{children}</ContentLayout> */}
+        <HomeHeader />
+        {children}
       </AdminPanelLayout>
     </div>
   );
--- web/app/home/page.tsx
+++ web/app/home/page.tsx
@@ -2,5 +2,5 @@ import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
 import { ContentLayout } from "@/components/admin-panel/content-layout";
 
 export default function Home() {
-  return <div>Content</div>;
+  return <div></div>;
 }
