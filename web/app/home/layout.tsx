8e17fefd8e3d8c39cb3d06fd623160001e27f42e
chixx <lucottejeremie@gmail.com>
2024-10-08 16:11:19 +0200

    update home layout with responsive

--- web/app/home/layout.tsx
+++ web/app/home/layout.tsx
@@ -10,7 +10,9 @@ export default function Layout({ children }: Props) {
     <div className="w-screen h-screen overflow-hidden">
       <AdminPanelLayout>
         <HomeHeader />
-        <div className="h-[calc(100vh_-_188px)] w-full">{children}</div>
+        <div className="h-[calc(100vh_-_168px)] sm:h-[calc(100vh_-_188px)] w-full">
+          {children}
+        </div>
       </AdminPanelLayout>
     </div>
   );
