f05b787e571bb7dc5cdd1147ada55d252aa4ba95
chixx <lucottejeremie@gmail.com>
2024-10-08 16:43:55 +0200

    update home page with glimmer grabs

--- web/app/home/page.tsx
+++ web/app/home/page.tsx
@@ -1,6 +1,12 @@
-import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
-import { ContentLayout } from "@/components/admin-panel/content-layout";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import GlimmerGrabs from "@/components/highlights/glimmer-grabs";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="h-full w-full flex flex-col">
+      <ScrollArea className="flex-1 px-4 sm:px-8">
+        <GlimmerGrabs />
+      </ScrollArea>
+    </div>
+  );
 }
