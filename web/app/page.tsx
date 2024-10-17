42694a2f44b362aeed1bfb0ac616899e2e64cf62
chixx <lucottejeremie@gmail.com>
2024-10-17 18:57:45 +0200

    update landing page for ripple integration

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,31 +3,52 @@ import TextRevealByWord from "@/components/ui/text-reveal";
 import Image from "next/image";
 import GlitteringSea from "@/components/ui/glittering-sea";
 import MetaballsAnimation from "@/components/ui/metaballs-animation";
+import Ripple from "@/components/ui/ripple";
+import AnimatedShinyText from "@/components/ui/animated-shiny-text";
+import { ChevronRight } from "lucide-react";
+
+import Link from "next/link";
+import MainPage from "@/components/landing-page/main-page";
+
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen">
-      <div className="flex h-full items-center justify-center flex-col">
-        <Card className="z-20 bg-white/60 rounded-none border-black border-4">
+      <div className="flex w-full h-full items-center justify-center flex-col">
+        {/* <Card className="z-20 bg-white/60 rounded-none border-black border-4">
           <div className="flex items-center justify-center w-full h-full p-4">
             <h1 className="text-4xl font-extrabold text-center text-black">
               BETTER
             </h1>
           </div>
-        </Card>
+        </Card> */}
         {/* <h1 className="mt-8 text-center font-custom text-3xl">
           A Stronger bond
         </h1> */}
-        <div className="z-20 p-4 items-center justify-center">
+        {/* <div className="z-20 p-4 items-center justify-center">
           <Image
             src="/stronger-bond.svg"
             alt="A stronger bond"
             width={250}
             height={90}
           ></Image>
-        </div>
+        </div> */}
+        {/* <div
+          className={cn(
+            "z-20 group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
+          )}
+        >
+          <Link href="/home">
+            <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+              <span>✨ Open APP</span>
+              <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+            </AnimatedShinyText>
+          </Link>
+        </div> */}
         {/* <GlitteringSea /> */}
-        <MetaballsAnimation />
+        {/* <MetaballsAnimation /> */}
+        <MainPage />
       </div>
       <div className="flex h-[200vh] items-center justify-center">
         <TextRevealByWord
